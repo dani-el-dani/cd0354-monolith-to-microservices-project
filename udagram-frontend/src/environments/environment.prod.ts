a149d523891f6571da0ba65766f8b289a436c563
dani-el-dani <getdani11@gmail.com>
2022-09-17 08:27:12 +0300

    deployment files added

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a5a0a2e01f7ea436f8ee3ee181c43dc0-1948320056.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
